ed7b0c5cc590c6f2fb2c90c8336704d02b712413
DrMohammad1 <dastoorimrmohammad@gmail.com>
2024-09-12 00:00:56 +0330

    tamrin 6

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,16 @@
-// write your func here
+function areArraysEqual(arr1, arr2) {  
+  if (arr1.length !== arr2.length) {  
+    return false;  
+  }  
+  
+  for (let i = 0; i < arr1.length; i++) {  
+    if (arr1[i] !== arr2[i]) {  
+      return false;  
+    }  
+  }  
+ 
+  return true;  
+}  
+  
+console.log(areArraysEqual([12, 1, 8, 20, 5], [12, 1, 8, 20, 5]));
+console.log(areArraysEqual([12, 1, 8, 20, 5], [12, "1", 8, 20, 5]));
