717fc5b42b30cba679b25027497c597f919158f2
DrMohammad1 <dastoorimrmohammad@gmail.com>
2024-09-11 23:44:50 +0330

    tamrin 3

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,10 @@
-// write your func here
+function factorial(n) {  
+  if (n === 0) {  
+    return 1;  
+  }  
+ 
+  return n * factorial(n - 1);  
+}  
+ 
+console.log(factorial(3));
+console.log(factorial(12));
