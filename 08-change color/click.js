677311162a1a9f01672b8300ee6c86d862dc10e1
DrMohammad1 <dastoorimrmohammad@gmail.com>
2024-09-12 00:20:54 +0330

    Update click.js

--- 08-change color/click.js
+++ 08-change color/click.js
@@ -1,5 +1,5 @@
 const colorBoxes = document.querySelectorAll('body > div');  
-
+ 
 colorBoxes.forEach(box => {  
   box.addEventListener('click', changeBoxColor);  
 });  
@@ -8,6 +8,6 @@ function changeBoxColor() {
   const r = Math.floor(Math.random() * 256);  
   const g = Math.floor(Math.random() * 256);  
   const b = Math.floor(Math.random() * 256);  
- 
+
   this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;  
 }
