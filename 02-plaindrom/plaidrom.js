08f641ae2d502f65c9602f61aa6d4482ae47b158
DrMohammad1 <dastoorimrmohammad@gmail.com>
2024-09-11 23:34:12 +0330

    tamrin 3

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,11 @@
-// write your func here
+function isPalindrome(str) {    
+    str = str.replace(/\s/g, '').toLowerCase();  
+    
+    let reversedStr = str.split('').reverse().join('');  
+    
+    return str === reversedStr;  
+  }  
+    
+  console.log(isPalindrome("headphone"));  
+  console.log(isPalindrome("racecar"));
+  
