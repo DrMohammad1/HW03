c4e46592cc8a36dac599acbb9bbe7b77181d0d56
DrMohammad1 <dastoorimrmohammad@gmail.com>
2024-09-12 00:43:00 +0330

    tamrin 8

---
+++ 08-change color/click.js
@@ -0,0 +1,14 @@
+const colorBoxes = document.querySelectorAll('body > div');  
+ 
+colorBoxes.forEach(box => {  
+  box.addEventListener('click', changeBoxColor);  
+});  
+
+function changeBoxColor() {  
+  const r = Math.floor(Math.random() * 256);  
+  const g = Math.floor(Math.random() * 256);  
+  const b = Math.floor(Math.random() * 256);  
+
+  this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;  
+ 
+}
